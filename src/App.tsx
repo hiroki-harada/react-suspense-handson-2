addf805a4ef323d5ebdf7ba6910fd53621f929c7
hiroki-harada <poketgame.0604@gmail.com>
2022-06-14 23:39:03 +0000

    [add] finish chap2

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,30 @@
+import { startTransition, Suspense, useState } from 'react'
+import { Sleep1s } from './component/Sleep1s'
+
 import './App.css'
 
 function App() {
+  const [sleepIsShown, setSleepIsShown] = useState(false);
   return (
     <div className="text-center">
       <h1 className="text-2xl">React App!</h1>
+      <Suspense fallback={<p>Loading...</p>}>
+        {sleepIsShown ? <Sleep1s /> : null}
+      </Suspense>
+      <p>
+        <button
+          className="border p-1"
+          onClick={() => {
+            startTransition(() => {
+              setSleepIsShown(true);
+            });
+          }}
+        >
+          Show Sleep1s
+        </button>
+      </p>
     </div>
-  )
+  );
 }
 
 export default App
